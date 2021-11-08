435f138d5e1c9278f62ef015da90b71ed6a87faa
Steeve VERDIEU <pixelsplats@gmail.com>
2021-11-08 10:45:01 -0500

    Clean solution : Apply Functional Programming to Convert Strings to URL Slugs

--- JavaScript Algorithms and Data Structures/8-Functional Programming/21-apply-functional-programming-to-convert-strings-to-url-slugs.js
+++ JavaScript Algorithms and Data Structures/8-Functional Programming/21-apply-functional-programming-to-convert-strings-to-url-slugs.js
@@ -1,6 +1,20 @@
-// Only change code below this line
+// For the challenge : "Apply Functional Programming to Convert Strings to URL Slugs"
+
+/**
+ * Solution
+ * 
+ * @param {*} title 
+ * @returns 
+ */
 function urlSlug(title) {
+  
+  // Lowercase all the letters
+  // The convert the sentence into array
+  // Then delete all extra spaces caracters that contains this array
+  // The convert the array into a sentence again
   return title.toLowerCase().split(" ").filter(word => word != "").join("-");
 }
-// Only change code above this line
+
+// Output Tests
+
 console.log(urlSlug(" Winter Is  Coming"))
