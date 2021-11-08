084bbab169712dca9b9acdf184efb0a2c0288a4d
Steeve VERDIEU <pixelsplats@gmail.com>
2021-11-08 10:22:22 -0500

    Clean solution for Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem

--- JavaScript Algorithms and Data Structures/8-Functional Programming/16-use-higher-order-functions-map-filter-or-reduce-to-solve-a-complex-problem.js
+++ JavaScript Algorithms and Data Structures/8-Functional Programming/16-use-higher-order-functions-map-filter-or-reduce-to-solve-a-complex-problem.js
@@ -1,17 +1,48 @@
+// Use one of these two solutions
+// For the challenge : "Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem"
+
+
+/**
+ * Solution 1
+ * I use the "filter" method to implement this solution
+ * 
+ * @param {*} arr 
+ * @returns {Object}
+ */
 const squareList = arr => {
-  // Only change code below this line
-  /*return arr.filter(n => Number.isInteger(n) && n > 0)
-            .map(n => n * n);*/
+
+  // Use filter to filter all the positive integers
+  // The map (modified) the filtered array
+  // To put the square of each item
+  return arr.filter(n => Number.isInteger(n) && n > 0)
+            .map(n => n * n);
+
+};
+
+/**
+ * Solution 2
+ * I use the "reduce" method to implement this solution
+ * 
+ * @param {*} arr 
+ * @returns {Object}
+ */
+ 
+const squareList = arr => {
   
+  // Use reduce to loop through the array
+  // Then check all the positive Integer in the array
+  // Push the square of these integers into "myArray"
   return arr.reduce((myArray, n) => {
     if(Number.isInteger(n) && n > 0) {
       myArray.push(n * n)
     }
     return myArray;
-  }, [])
-  // Only change code above this line
+  }, []);
+
 };
+
+
+// Output Tests
 
 const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
 console.log(squaredIntegers);
-console.log(typeof 8.9)
