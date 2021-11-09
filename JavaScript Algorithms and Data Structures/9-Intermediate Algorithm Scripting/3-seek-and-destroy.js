a5e4d464bca624381664d7dc994cfcf4fa448779
Steeve VERDIEU <pixelsplats@gmail.com>
2021-11-09 08:29:27 -0500

    Correction: Seek and Destroy

--- JavaScript Algorithms and Data Structures/9-Intermediate Algorithm Scripting/3-seek-and-destroy.js
+++ JavaScript Algorithms and Data Structures/9-Intermediate Algorithm Scripting/3-seek-and-destroy.js
@@ -1,14 +1,24 @@
+// For the challenge : Seek and Destroy
+
+/**
+ * Solution
+ * 
+ * @param {*} arr 
+ * @returns 
+ */
 function destroyer(arr) {
+
   // Get the other arguments
-  // That are not the array "arr" argument
+  // That are not in the array "arr" argument
   // Put them in an array
   let removeEls = [...arguments].filter((item, i) => i != 0)
 
   // Get all the elements
-  // That are not in the array of  removeEls
+  // That are not in the array of removeEls
   return arr.filter(item => {
     return !removeEls.includes(item);
   });
+
 }
 
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
