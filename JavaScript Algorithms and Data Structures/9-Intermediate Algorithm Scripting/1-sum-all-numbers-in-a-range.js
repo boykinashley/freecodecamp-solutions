005cdc308269d7ffd6cd7584d3abc9323c0c47e3
Steeve VERDIEU <pixelsplats@gmail.com>
2021-11-09 08:20:23 -0500

    Correction : Sum All Numbers in a Range

--- JavaScript Algorithms and Data Structures/9-Intermediate Algorithm Scripting/1-sum-all-numbers-in-a-range.js
+++ JavaScript Algorithms and Data Structures/9-Intermediate Algorithm Scripting/1-sum-all-numbers-in-a-range.js
@@ -1,18 +1,59 @@
+// For the challenge : Sum All Numbers in a Range
+// Use one of these two solutions
+
+/**
+ * Solution 1
+ * Use a for Loop in this solution
+ * 
+ * @param {*} arr 
+ * @returns 
+ */
 function sumAll(arr) {
+
+  // Sort the array to put the smaller number
+  // in the first index of the array
   arr.sort((a, b) => a < b ? -1 : 1);
 
+  // Deconstructing the array
+  // To put the numbers into variables : a & b
   const [a, b] = arr;
-  /** Solution with for loop */
-  //let sum = 0;
-  //for(let i = a; i <= b; i++) {
-  //  sum +=i;
-  //}
-  // return sum;
 
+  // Iterate from a to b
+  // And sum up all the numbers between them
+  let sum = 0;
+  for(let i = a; i <= b; i++) {
+    sum +=i;
+  }
 
-  /* Solution with recursion */
+  return sum;
+
+}
+
+/**
+ * Solution 2
+ * Use a for recursion in this solution
+ * 
+ * @param {*} arr 
+ * @returns 
+ */
+ function sumAll(arr) {
+
+  // Sort the array to put the smaller number
+  // in the first index of the array
+  arr.sort((a, b) => a < b ? -1 : 1);
+
+  // Deconstructing the array
+  // To put the numbers into variables : a & b
+  const [a, b] = arr;
+
+  // Use recursion
+  // To sum from a to b
+  // by iterate the variable a
+  // each time this function called itself
   if(a == b) return a;
   return a + sumAll([a + 1, b])
 }
+
+// Output Test
 
 console.log(sumAll([4, 1]));
